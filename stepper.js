0a1e95acbde31659cf5809ead4c555c8ba10adbc
Nathan Broadbent <nate@manchester.com>
2017-01-21 11:47:45 +0000

    Refactoring. Added utility functions.

--- stepper.js
+++ stepper.js
@@ -1,48 +1,85 @@
+// Config
 var gearRatio = 64;
 var steps = 32;
+var rpm = 1200;
+var GPIOpins = [17, 18, 21, 22];
 
+// Init
 var StepperWiringPi = require("stepper-wiringpi");
 
-var motor = StepperWiringPi.setup( steps, 17, 18, 21, 22);
+// Setup 
+var motor = StepperWiringPi.setup(
+	steps,
+	GPIOpins[0],
+	GPIOpins[1],
+	GPIOpins[2],
+	GPIOpins[3]
+);
+motor.setSpeed(rpm);
 
-motor.setSpeed(1200);
-
+// Useful variables
 var stepsPerRev = ( steps * gearRatio );
-var stepPerDegree = stepsPerRev  / 360;
+var stepsPerDegree = stepsPerRev  / 360;
 
+// TODO: Read current step from disk, to ensure motor starts at step 0.
+
+// Used to store current step.
 var currentStep = 0;
 
-stepToDeg( 90, function( currentStep ){
-	console.log("Final callback!" + currentStep);
+// TEST - Move to angle ( degrees );
+moveToDeg( 90, function( degree ){
+	console.log("Move completed to degree " + degree );
 });
 
-function stepToDeg( degree, callback ){
+// Move Stepper Motor to Angle (degrees)
+function moveToDeg( degree, callback ){
+	// Stop motor.
 	motor.halt();
 
-	var steps = degree * stepPerDegree;
-	currentStep = 0;
+	var steps = degToStep( degree );
+	console.log("moveToDeg() - Begin stepping to " + degree + " degrees / " + steps + " steps." );
 
-	console.log("stepToDeg() - Stepping to " + degree + " degrees - " + steps + " steps." );
-
-	motorStep( steps, callback );
+	motorStepTo( steps, callback );
 }
 
-function motorStep( steps, callback ){
+// Move Stepper Motor To X  Steps.
+function motorStepTo( steps, callback ){
 
+	// Recursive function, move to X steps 1 step at a time.
 	var stepFunc = function(){
 
+		// TODO: Add support for both directions.
+
+		// Step by 1.
 		motor.step( 1, function(){
 			currentStep += 1;
 
+			// Check if completed.
 			if( currentStep == steps ){
-				console.log( "stepToDeg() - Completed - Steps = " + steps + " Current Step = " + currentStep );
+
+				console.log( "stepToDeg() - Completed - " + stepToDeg( steps  ) + " degrees | Actual - " + stepToDeg( currentStep ) + " degrees.");
+				// Finished callback.
 				callback( currentStep );
 			} else {
+				// Not completed, move another step.
 				stepFunc();
 			}
 
+			// Normalise
 			if( currentStep >= stepsPerRev ) currentStep = 0; // Test for drift.
 		});
 	};
+
+	// Start moving...
 	stepFunc();
+}
+
+// Convert Steps to Degree
+function stepToDeg( steps ){
+	return ( steps / stepsPerRev ) * 360;
+}
+
+// Convert Degrees to Steps
+function degToStep( deg ){
+	return deg * stepsPerDegree;
 }
