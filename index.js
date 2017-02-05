ee5291b3127300fdcb41e93371e29f94b685700b
Nathan Broadbent <nate@manchester.com>
2017-02-05 16:36:10 +0000

    Added 'moveToAngle()' and function to test randomised angles.

--- index.js
+++ index.js
@@ -3,23 +3,97 @@ var stepper = require('./stepper');
 stepper.setup({
   gearRatio: 64,
   steps: 32,
-  rpm: 1200,
+  rpm: 99999,
   GPIOpins: [17, 18, 21, 22]
 });
 
+// Tests
+
+// Moves to random degree relative to starting position.
+testMoveToRandom();
+
+// Will move increments of 90 (e.g. 90, 180... ) relative to starting position.
+// testMoveToAngle();
+
+// Will move by 90 each time, relative to starting position.
+// testMoveByDeg();
+
+
+// ------------------------------
+
+
+
+// Test - Move to random angle every 1 second (e.g. don't wait for current movement to complete).
+function testMoveToRandom(){
+  var c = 0;
+
+  setInterval(function(){ 
+    c++;
+
+    var angle = getRandomInt( 0, 360 );
+
+    console.log( c + ". Start - Move to " + angle );
+
+    stepper.moveToAngle( angle, function( degree ){
+      // console.log( c + ". Completed @ " + degree );
+    });
+
+  }, 1000);
+}
+
+
+
+// Test - Move to specific angle, increments by 90, each time. Requires incrementing degree value input.
+function testMoveToAngle(){
+  var c = 0;
+  function moveToAngleBy90(){
+    c++;
+
+    var angle = 90 * c;
+
+    console.log( c + ". Start - Move to " + angle );
+
+    stepper.moveToAngle( angle, function( degree ){
+
+      // console.log( c + ". Completed @ " + degree );
+
+      setTimeout(function(){ 
+
+        moveToAngleBy90();
+
+      }, 2000);
+
+    });
+  }
+  moveToAngleBy90();
+}
+
 
 // Test - Move in increments of 90 degrees.
-var c = 0;
-function moveBy90(){
-  stepper.moveByDeg( 90, function( degree ){
-    console.log( (++c) + ". Completed @ " + degree );
+function testMoveByDeg(){
+  var c = 0;
+  function moveBy90(){
+    stepper.moveByDeg( 90, function( degree ){
+      console.log( (++c) + ". Completed @ " + degree );
 
-    setTimeout(function(){ 
+      setTimeout(function(){ 
 
-      moveBy90();
+        moveBy90();
 
-    }, 2000);
+      }, 2000);
 
-  });
+    });
+  }
+  moveBy90();
 }
-moveBy90();
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomFloat(min, max) {
+  return Math.random() * (max - min) + min;
+}
--- stepper.js
+++ stepper.js
@@ -2,6 +2,7 @@ var StepperWiringPi = require("stepper-wiringpi");
 
 var Stepper = {
 	setup: setup,
+	moveToAngle: moveToAngle,
 	moveByDeg: moveByDeg,
 	motor: null,
 	gearRatio: null,
@@ -35,7 +36,39 @@ function setup( params ){
 	Stepper.motor.setSpeed( Stepper.rpm );
 }
 
-// Move Stepper Motor to Angle (degrees)
+// TODO: Handling start position
+// Make sure start position is always 'north'.
+// Store stepper position as it moves (to disk), use this to reset start position at boot up.
+// Can also store position when crash, exit, etc/
+
+// 1. move stepper
+// 2. store position.
+// --- New session.
+// 1. check position.
+// 2. move to 0.
+
+
+// Move To Angle = Move stepper to absolute angle, taking into account current position.
+function moveToAngle( degree, callback ){
+	// Stop motor.
+	Stepper.motor.halt();
+
+	this.steps = degToStep( degree );
+	console.log("moveToAngle() - Begin stepping to " + degree + " degrees / " + this.steps + " steps." );
+
+	console.log( "Steps to move:" + this.steps );
+
+	// Calc the quickest direction to rotate to new angle.
+	var currentAngle = stepToDeg( Stepper.currentStep );
+	var targetAngle = degree;
+	var direction = shortestDirection( currentAngle,  targetAngle );
+
+	console.log( currentAngle, targetAngle, direction );
+
+	motorStepTo( this.steps, callback, direction );
+}
+
+// Move By Degree - Move Stepper Motor by specific amount of degrees, with no regard to motors current position.
 function moveByDeg( degree, callback ){
 	// Stop motor.
 	Stepper.motor.halt();
@@ -48,28 +81,27 @@ function moveByDeg( degree, callback ){
 
 	console.log( "Steps to move:" + this.steps );
 
-	motorStepTo( this.steps, callback );
+	motorStepTo( this.steps, callback, 1 );
 }
 
 // Move Stepper Motor To X  Steps.
-function motorStepTo( steps, callback ){
+function motorStepTo( steps, callback, direction ){
 
 	// Recursive function, move to X steps 1 step at a time.
 	var stepFunc = function(){
 
-		// TODO: Add support for both directions.
-
 		// TODO: Issues with normalised step, vs actual step.
 
-		// Step by 1.
-		Stepper.motor.step( 1, function(){
+		// Step by '1 step' at a time.
+		Stepper.motor.step( direction, function(){
 
 			Stepper.currentStep += 1;
 
 			// Check if completed.
-			if( Stepper.currentStep >= steps ){
+			if( Stepper.currentStep == steps ){
 
-				console.log( "stepToDeg() - Completed - " + stepToDeg( steps  ) + " degrees | Actual - " + stepToDeg( Stepper.currentStep ) + " degrees.");
+				//console.log( "stepToDeg() - Completed - " + stepToDeg( steps  ) + " degrees | Actual - " + stepToDeg( Stepper.currentStep ) + " degrees.");
+				
 				// Finished callback.
 				callback( stepToDeg( Stepper.currentStep ) );
 			} else {
@@ -79,8 +111,6 @@ function motorStepTo( steps, callback ){
 
 			// Normalise
 			Stepper.currentStep = clampSteps( Stepper.currentStep );
-
-			//if( Stepper.currentStep >= Stepper.stepsPerRev ) Stepper.currentStep = 0; // Test for drift.
 		});
 	};
 
@@ -108,5 +138,19 @@ function stepToDeg( steps ){
 
 // Convert Degrees to Steps
 function degToStep( deg ){
-	return deg * Stepper.stepsPerDegree;
+	// 360 needs to return total steps per 1 reolution, rather than being clamped to 0.
+	if( deg == 360 ) return Stepper.stepsPerRev;
+
+	deg = clampAngle( deg );
+	return Math.round( deg * Stepper.stepsPerDegree );
+}
+
+// Will calc the quickest rotation to target angle (returns sign based on direction)
+function shortestRotation( current, target ){
+	var a = target - current;
+	return (a + 180) % 360 - 180;
+}
+
+function shortestDirection( current, target ){
+	return shortestRotation( current,  target ) > 0 ? 1 : -1;
 }
